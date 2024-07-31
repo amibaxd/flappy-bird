c4ed6f929203bfb4ad683c1f1a0d0e1106f4ecfb
Mithun <mithun.visu@gmail.com>
2024-07-31 22:13:08 +0530

    implemented dynamic speed change based on current score

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class GameManager : MonoBehaviour
 {
@@ -47,5 +48,22 @@ public class GameManager : MonoBehaviour
     public void AddScore(int _score)
     {
         score += _score;
+
+        MoveLeft[] moveLeftScripts = FindObjectsOfType<MoveLeft>();
+
+        foreach (MoveLeft moveLeft in moveLeftScripts)
+        {
+            if (score <= 50)
+            {
+                // Linear increase from 5 to 15 over 50 iterations
+                moveLeft.speed = 5 + (score - 1) * (15 - 5) / 49.0; // Linearly interpolate from 5 to 15
+            }
+            else
+            {
+                // Logarithmic increase from 15 to 20 over the next 50 iterations
+                int adjustedI = score - 50; // Adjust index for the logarithmic phase
+                moveLeft.speed = moveLeft.baseSpeed + (moveLeft.endSpeed - moveLeft.baseSpeed) * (Math.Log(adjustedI + 1) / Math.Log(50));
+            }
+        }
     }
 }
--- Assets/Scripts/Core/MoveLeft.cs
+++ Assets/Scripts/Core/MoveLeft.cs
@@ -1,13 +1,26 @@
+using System;
 using UnityEditor.Rendering;
 using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    [SerializeField] private float speed;
+    public double speed;
+    public double baseSpeed;
+    public double endSpeed;
+
+
+    private void Awake()
+    {
+        speed = 5;
+        baseSpeed = 15;
+        endSpeed = 20;
+    }
 
     private void Update()
     {
         if(!GameManager.instance.isGameOver)
-            transform.Translate(new Vector2(-speed * Time.deltaTime, 0));
+            transform.Translate(new Vector2((float)-speed * Time.deltaTime, 0));
+
+        Debug.Log(speed);
     }
 }
